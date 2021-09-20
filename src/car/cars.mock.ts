4ddae801f1918f3e483beb49545c403a1c25bade
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-20 17:23:26 +0530

    :sparkles: Create Cars mock data

---
+++ src/car/cars.mock.ts
@@ -0,0 +1,14 @@
+export const Cars = [
+    {
+        id: '10022718',
+        brand: 'BMW',
+        color: 'Gold',
+        model: 'BMW X5'
+    },
+    {
+        id: '10022710',
+        brand: 'Sabaru',
+        color: 'Yellow',
+        model: 'Leagacy'
+    }
+]
